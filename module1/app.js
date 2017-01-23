6d74ef52e171d4f92d9087af70d68eff425253bf
Enrique Condes <enrique@shapeoko.com>
2017-01-23 00:53:02 -0600

    Module 1 Solution + first 2 bonuses

--- module1/app.js
+++ module1/app.js
@@ -8,17 +8,23 @@ LunchCheckController.$inject =['$scope'];
 function LunchCheckController ($scope) {
   $scope.message = "";
   $scope.lunch="";
+  $scope.color="black";
+  $scope.borderColor="";
   $scope.checkLunch = function () {
     if ($scope.lunch=="")
     {
       $scope.message = "Please enter data first";
+      $scope.color ="red";
+      $scope.borderColor="red";
     }
     else {
+      $scope.color ="green";
       if ($scope.lunch.split(',').length > 3) {
         $scope.message = "Too much!";
       } else {
         $scope.message = "Enjoy!";
       }
+      $scope.borderColor="green";
     }
   }
 }
